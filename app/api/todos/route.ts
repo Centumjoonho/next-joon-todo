b2120057092c67075f1c5398177a5e7c6d81170a
Centumjoonho <ghwnsgkgk@kakao.com>
2023-11-03 14:26:16 +0900

    2023-11-03

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,31 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 import dummyTodos from "@/data/dummy.json";
+import { fetchTodos, addTodos } from "@/data/firestore";
 
 /// 모든 할일 가져오기
 export async function GET(request: NextRequest) {
+  const fetchedTodos = await fetchTodos();
+
   const response = {
     message: "할일 모두 가져오기",
-    data: dummyTodos,
+    data: fetchedTodos,
   };
 
   return NextResponse.json(response, { status: 200 });
 }
-
+// 할일 추가하기
 // NextRequest 문서 확인 필 !
 export async function POST(request: NextRequest) {
   //   const data = await request.json();
 
   const { title } = await request.json();
 
-  const newTodo = {
-    id: "10",
-    title: title,
-    is_done: false,
-  };
+  const addedTodo = await addTodos({ title });
 
   const response = {
     message: " 할일 추가 성공",
-    data: newTodo,
+    data: addedTodo,
   };
 
   return Response.json(response, { status: 201 });
--- data/firestore.js
+++ data/firestore.js
@@ -1,21 +1,105 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
+import { getFirestore, collection, getDocs, doc, updateDoc, setDoc, Timestamp, getDoc } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: ,
-    measurementId: 
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
+//모든 할일 가져오기 
+export async function fetchTodos() {
+
+    const querySnapshot = await getDocs(collection(db, "centum-todos"));  //DB 이름
+
+    if (querySnapshot.empty) {
+        return [];
+    }
+
+    const fetchedTodos = [];
+
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+
+        const aTodo = {
+            id: doc.id,
+            title: doc.data()["title"],
+            is_done: doc.data()["is_done"],
+            created_at: doc.data()["created_at"].toDate(),
+        }
+        //.toLocaleTimeString('ko')
+        fetchedTodos.push(aTodo);
+    });
+    return fetchedTodos;
+}
+
+
+//할일 추가
+export async function addTodos({ title }) {
+
+
+    // Add a new document with a generated id
+    const newTodoRef = doc(collection(db, "centum-todos"));
+
+    const createdAtTimestamp = Timestamp.fromDate(new Date());
+
+    const newTodoData = {
+        id: newTodoRef.id,
+        title: title,
+        is_done: false,
+        created_at: createdAtTimestamp,
+    }
+    // later...
+    await setDoc(newTodoRef, newTodoData);
+    return newTodoData;
+}
+
+
+//단일 할일 조회
+export async function fetchTodo(id) {
+
+
+    const docRef = doc(db, "cities", "SF");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+    } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+    }
+
+}
+
+
+
+
+module.exports = { fetchTodos, addTodos }
+
+
+
+
+
+
+
+
